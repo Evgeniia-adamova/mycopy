7e18164fe7e7b9511aac01097d70337a4c6c7f9c
Evgeniia Adamova <st114255@student.spbu.ru>
2024-11-13 16:15:49 +0000

    Evgeniia's file

--- task2.py
+++ task2.py
@@ -1,5 +1,15 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def power_calc(x, y):
+    if y <= 1:
+        return x == 1
+    if x < 1:
+        return False
+
+    while x > 1:
+        if x % y != 0:
+            return False
+        x = x // y
+
+    return x == 1
+
+
+print(power_calc(16, 2))
